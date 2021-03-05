0408ec0805bf6fd2c0045fff2bbafc7763012881
calderprulhiere <60994457+calderprulhiere@users.noreply.github.com>
2021-03-05 14:44:03 -0800

    Add files via upload

---
+++ Project-2021Winter-Prulhiere-Calder/form.js
@@ -0,0 +1,15 @@
+function bindButtons() {
+    document.getElementById('submit_input').addEventListener('click', function(event) {
+        var request = new XMLHttpRequest();
+        var result;
+
+
+}
+
+
+// init
+function init() {
+    document.addEventListener('DOMContentLoaded', bindButtons);
+}
+
+init();
